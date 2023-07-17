252b620de471fa37b83bf2d517bacd3b68685785
Agenteec <g.mens.hl@gmail.com>
2023-07-18 01:33:16 +0300

    use argv in util

--- main.cpp
+++ main.cpp
@@ -24,28 +24,42 @@ void showLoadingIndicator() {
 
     }
     std::cout << "\r" << ANSI_COLOR_GREEN<< "Loading Complete!" << std::endl;
+    std::cout << ANSI_COLOR_RESET;
 }
 
 int main(int argc, char* argv[]) {
-    std::thread loadingThread(showLoadingIndicator);
+    if (argc < 4) {
+        std::cout << "Usage: " << argv[0] << " <branch1> <branch2> <path/filename>" << std::endl;
+        std::cout << "For example pkgcompare_cli p10 p9 C:/output" << std::endl;
+        return 1;
+    }
+
+    std::string branch1 = argv[1];
+    std::string branch2 = argv[2];
+    std::string filename = argv[3];
     PackageComparison pkgComparison;
     PackageComparisonResult result;
-    result = pkgComparison.comparePackages("sisyphus", "p10", "123");
-    loading = false;
-    loadingThread.join();
-    std::cout << "Packages only in branch1:" << std::endl;
-    for (const std::string& package : result.packagesOnlyInBranch1) {
-        std::cout << package << std::endl;
-
-    }
-    std::cout << "Packages only in branch2:" << std::endl;
-    for (const std::string& package : result.packagesOnlyInBranch2) {
-        std::cout << package << std::endl;
-    }
-
-    std::cout << "Packages with higher version in branch1:" << std::endl;
-    for (const std::string& package : result.packagesWithHigherVersionInBranch1) {
-        std::cout << package << std::endl;
-    }
+    result = pkgComparison.comparePackages(branch1, branch2, filename);
     return 0;
 }
+
+
+#pragma region Debug
+//std::thread loadingThread(showLoadingIndicator);
+//loading = false;
+//loadingThread.join();
+//std::cout << "Packages only in branch1:" << std::endl;
+//for (const std::string& package : result.packagesOnlyInBranch1) {
+//    std::cout << package << std::endl;
+
+//}
+//std::cout << "Packages only in branch2:" << std::endl;
+//for (const std::string& package : result.packagesOnlyInBranch2) {
+//    std::cout << package << std::endl;
+//}
+
+//std::cout << "Packages with higher version in branch1:" << std::endl;
+//for (const std::string& package : result.packagesWithHigherVersionInBranch1) {
+//    std::cout << package << std::endl;
+//}
+#pragma endregion
