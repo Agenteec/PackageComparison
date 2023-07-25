faa14eda73d0d5d7a4af7a101e6883df376b5d08
Agenteec <g.mens.hl@gmail.com>
2023-07-25 15:07:31 +0300

    link jsoncpp fix

--- main.cpp
+++ main.cpp
@@ -2,30 +2,6 @@
 
 #include "PackageCompare.h"
 #include <iostream>
-#include <chrono>
-#include <thread>
-
-
-bool loading = true;
-void showLoadingIndicator() {
-    const std::string animation = "|/-\\";
-    int animationIndex = 0;
-
-    while (loading) {
-
-        std::cout << "\r";
-
-        std::cout << ANSI_COLOR_YELLOW<< "Loading " << animation[animationIndex];
-
-        animationIndex = (animationIndex + 1) % animation.size();
-        std::cout << ANSI_COLOR_RESET;
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-
-    }
-    std::cout << "\r" << ANSI_COLOR_GREEN<< "Loading Complete!" << std::endl;
-    std::cout << ANSI_COLOR_RESET;
-}
 
 int main(int argc, char* argv[]) {
     if (argc < 4) {
@@ -42,24 +18,3 @@ int main(int argc, char* argv[]) {
     result = pkgComparison.comparePackages(branch1, branch2, filename);
     return 0;
 }
-
-
-#pragma region Debug
-//std::thread loadingThread(showLoadingIndicator);
-//loading = false;
-//loadingThread.join();
-//std::cout << "Packages only in branch1:" << std::endl;
-//for (const std::string& package : result.packagesOnlyInBranch1) {
-//    std::cout << package << std::endl;
-
-//}
-//std::cout << "Packages only in branch2:" << std::endl;
-//for (const std::string& package : result.packagesOnlyInBranch2) {
-//    std::cout << package << std::endl;
-//}
-
-//std::cout << "Packages with higher version in branch1:" << std::endl;
-//for (const std::string& package : result.packagesWithHigherVersionInBranch1) {
-//    std::cout << package << std::endl;
-//}
-#pragma endregion
